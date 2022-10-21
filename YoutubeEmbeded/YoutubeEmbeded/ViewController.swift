a7b17aa12c97a9531f642ded323ee7b324f71aa2
yexjin <tnska0421@naver.com>
2022-10-21 16:28:00 +0900

    [CHORE] .ignore file

---
+++ YoutubeEmbeded/YoutubeEmbeded/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeEmbeded
+//
+//  Created by 오예진 on 2022/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
