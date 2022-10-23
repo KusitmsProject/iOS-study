47d1c51f300daab7415353c0ef6dc4b9dc8a83ad
yexjin <tnska0421@naver.com>
2022-10-23 17:11:13 +0900

    [APITest] init

---
+++ APITest/APITest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APITest
+//
+//  Created by 오예진 on 2022/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
