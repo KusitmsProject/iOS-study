e32e4eacc6434c7c81a52ebb51faf15f757bf518
yexjin <tnska0421@naver.com>
2022-10-26 07:39:18 +0900

    [PushAlarm] init

---
+++ PushAlarm/PushAlarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PushAlarm
+//
+//  Created by 오예진 on 2022/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
