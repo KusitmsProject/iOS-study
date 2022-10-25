fbd88694a95a38f030fbcf869b93de4350c430c7
yexjin <tnska0421@naver.com>
2022-10-25 13:10:44 +0900

    [KakaoLogin] init

---
+++ KakaoLogin/KakaoLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KakaoLogin
+//
+//  Created by 오예진 on 2022/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
